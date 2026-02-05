d5786dfe0e7c49735b416600317f74e576e7008a
Ivo  Bryan <ivobryan12@gmail.com>
2026-02-05 07:56:59 +0200

    Stagger list in framer motion animated �

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { motion } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const names = ["Ivo", "Bryan", "Shema", "Ntwali"];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <motion.ul
+      variants={list}
+      initial="hidden"
+      animate="visible"
+      style={{ listStyle: "none", padding: 0 }}
+    >
+      {names.map((name) => (
+        <motion.li key={name} style={box} variants={item}>
+          {name}
+        </motion.li>
+      ))}
+    </motion.ul>
+  );
 }
 
-export default App
+const list = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.3 }, 
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, x: -50 },
+  visible: { opacity: 1, x: 0 },
+};
+
+const box = {
+  width: 80,
+  height: 80,
+  backgroundColor: "#ff0088",
+  margin: 10,
+  borderRadius: 5,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#fff",
+  fontWeight: "bold",
+};
